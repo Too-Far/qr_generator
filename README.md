879a2de7627d8aa4f93da93b07b29318beed16e2
Too-Far <rturner8404@gmail.com>
2020-01-12 03:29:15 -0500

    initial commit, added README and LICENSE

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # qr_generator
+
+Used to create QR codes. Able to modify code to create qr codes for whatever you need. 
+Some ideas include:
+    social media handles
+    websites
+    links to google maps
+    WIFI login info (the current iteration provided)
+Plans for the future:
+    I would like to incorporate this into a flask based application to allow users to input data to be made into a qr code. 
+
+Huge nod to the blog in Kite (https://kite.com/blog/python/creating-3d-printed-wifi-access-qr-codes-with-python/) for the tutorial. 
---
+++ generator.py
@@ -0,0 +1,23 @@
+'''QR codes are simply data turned into strings for machines to read.
+This program creates a QR code for a home wifi (to be able to give to guests, etc). You can make nearly anything with this program. IE, QR codes for your social media, websites, serial numbers, etc. 
+'''
+
+import pyqrcode as pq
+# import pypng
+
+
+ssid = 'HCpublic' #Put whatever the network name is here
+security = 'WPA' #Put whether the security is WPA or WEP
+password = '1234556847' #WIFI password here
+#Creates QR code 
+#qr = pq.create(f'WIFI:S:{ssid}; T: {security} ;P: {password};;')
+qr = pq.create(f'WIFI:S:{ssid};;')
+#Turns QR code into PNG for printing
+with open('WIFI.png', 'w') as fstream:
+    qr.png('WIFI.png', scale=5)
+
+#Can also create an SVG file:
+# qr.svg('WIFI.svg', scale=4)
+
+#Or simply print to terminal:
+# print(qr.terminal())
